c9deec219213645e1c8eac412a1a494781934462
林杨 <ly946546844@gmail.com>
2023-11-20 14:33:27 +0800

    初始化

---
+++ src/main/java/com/ly/datastatisticalanalysis/DataStatisticalAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.ly.datastatisticalanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStatisticalAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStatisticalAnalysisApplication.class, args);
+    }
+
+}
